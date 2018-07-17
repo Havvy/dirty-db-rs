d9296165ed18e998c705b3b18ac1167e5f39a017
Havvy <ryan.havvy@gmail.com>
2018-07-16 23:48:35 -0700

    Stubbed out basic functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,42 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::path::{Path, PathBuf};
+
+// FIXME: Use actual JSON type.
+type Json = ();
+
+pub struct DirtyDb {
+    path: PathBuf
+}
+
+impl DirtyDb {
+    pub fn new<P>(path: P) -> Result<DirtyDb, ()> where P: AsRef<Path> {
+        unimplemented!();
+    }
+
+    pub fn get_path(&self) -> &Path {
+        &self.path
+    }
+
+    // The state of the database for further accesses is updated instantly while
+    // the change will be written to disk shortly thereafter.
+    pub fn set(&mut self, key: String, value: Json) {
+        unimplemented!();
+    }
+
+    pub fn get(&self, key: String) -> Option<Json> {
+        unimplemented!();
+    }
+
+    pub fn remove(&self, key: String) {
+        unimplemented!();
+    }
+
+    // Explicitly closes the database. Same as dropping.
+    pub fn close(self) {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
